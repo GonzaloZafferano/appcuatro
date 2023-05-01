2d80e99c3e3dccfe7fb2a05794d2019d05b6ec01
zafferano.g <zafferano.g@edificarseguros.com.ar>
2023-04-30 23:52:17 -0300

    fin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'PruebaApp',
+  appId: 'memoria.starter',
+  appName: 'Memoria',
   webDir: 'www',
   bundledWebRuntime: false,
   plugins: {
